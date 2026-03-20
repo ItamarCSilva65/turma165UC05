e2331a77ce4bf9e2c0b08095eb7d6acc509db8a7
Itamar Silva <itamarcorreia2805@gmail.com>
2026-03-19 21:22:47 -0300

    Feat: Alteração do front

--- Aula02/front/src/pages/user/FilmesUsuarioPage.jsx
+++ Aula02/front/src/pages/user/FilmesUsuarioPage.jsx
@@ -7,6 +7,9 @@ import "./FilmesUsuarioPage.css";
 export default function FilmesUsuarioPage(){
      const [filmes, setFilmes] = useState([]);
      const [busca, setBusca] = useState("");
+     const [ordenacao, setOrdenacao] = useState("");
+     const [novaOrdenacao, setNovaOrdenacao] = useState ................
+
  async function carregarFilmes() {
         try {
             const response = await listarFilmes();
@@ -23,6 +26,65 @@ export default function FilmesUsuarioPage(){
     const filmesFiltrados = filmes.filter((filme)=>
     filme.titulo.toLowerCase().includes(busca.toLowerCase())
     );
+    const filmesOrdenados = [...filmesFiltrados].sort((a,b)=>{
+        if(ordenacao === "titulo-asc"){
+            return a.titulo.localeCompare(b.titulo)
+        }
+        if(ordenacao === "titulo-desc"){
+            return b.titulo.localeCompare(a.titulo)
+        }
+         if(ordenacao === "genero-asc"){
+            return a.genero.localeCompare(b.genero)
+        }
+        if(ordenacao === "genero-desc"){
+            return b.genero.localeCompare(a.genero)
+        }
+
+        if(ordenacao === "ano-asc"){
+            return a.ano - b.ano;
+        }
+        if(ordenacao === "ano-desc"){
+            return b.ano - a.ano;
+        }
+        return 0;
+
+    });
+
+    const filmesOrdenados = [...filmesFiltrados].sort((a, b)=> {...});
+
+    function alternarOrdenacao(campo){
+        setNovaOrdenacao((prev)=>{
+            if(prev.campo === campo){
+                return{
+                    campo,
+                    direcao: prev.direcao === "asc" ? "desc" : "asc"
+                }
+            }
+            return(
+                campo,
+                direcao: "asc"
+            )
+        })
+    }
+
+    const filmesOrdenados2 = [...filmesFiltrados].sort((a, b)=>{
+        if(novaOrdenacao.campo === "titulo"){
+            return novaOrdenacao.direcao === "asc" ?
+            a.titulo.localeCompare(b.titulo) :
+            b.titulo.localeCompare(a.titulo)
+        }
+
+         if(novaOrdenacao.campo === "genero"){
+            return novaOrdenacao.direcao === "asc" ?
+            a.genero.localeCompare(b.genero) :
+            b.genero.localeCompare(a.genero)
+        }
+         if(novaOrdenacao.campo === "ano"){
+            return novaOrdenacao.direcao === "asc" ?
+            a.ano.localeCompare(b.ano) :
+            b.ano.localeCompare(a.ano)
+        }
+    })
 
 
     return(
@@ -31,16 +93,55 @@ export default function FilmesUsuarioPage(){
             <section className="cabecalho-usuario">
                 <h1> Catalogo de Filmes</h1>
                 <p>Confira os filmes disponíveis no catálogo</p>
-                <div className="busca-box">
-                    <input
+                <div className="filtros-container">
+                    <div className="busca-box">
+                     <input
                         type="text"
                         value={busca}
                         placeholder="Buscar filme..."
                         onChange={(e)=> setBusca(e.target.value)}
                         />
                 </div>
+
+                </div>
+                <div className="ordenacao-botoes">
+                    <button className={ordenacao === "ano-asc" ? "ativo" : ""} onClick={() => setOrdenacao
+                        ("titulo-asc")}>
+                        Título A-Z
+                    </button>
+
+                    <button className={ordenacao === "ano-asc" ? "ativo" : ""} onClick={() => setOrdenacao
+                        ("titulo-desc")}>
+                        Título Z-A
+                    </button>
+
+                    <button className={ordenacao === "ano-asc" ? "ativo" : ""} onClick={()=> setOrdenacao
+                        ("genero-asc")}>
+                        Gênero A-Z
+                    </button>
+
+                    <button className={ordenacao === "ano-asc" ? "ativo" : ""} onClick={()=> setOrdenacao
+                        ("genero-desc")}>
+                        Gênero Z-A
+                    </button>
+
+                    <button className={ordenacao === "ano-asc" ? "ativo" : ""}
+                    onClick={()=> setOrdenacao ("ano-asc")}>
+                        Ano ⇡
+                    </button>
+
+                    <button className={ordenacao === "ano-desc" ? "ativo" : ""}
+                    onClick={()=> setOrdenacao ("ano-desc")}>
+                        Ano ↓
+                    </button>
+                    <button className="" onClick={()=> setOrdenacao
+                        ("")}>
+                        Limpar
+                    </button>
+        
+                </div>
             </section>
-              <FilmeList filmes={filmesFiltrados} />
+              <FilmeList filmes={filmesOrdenados} />
         </main>          
         </>
     )
